21c490f906d7910981581b713474666397eb5614
Edna2607 <eycardonaec@cesde.net>
2024-06-07 15:43:40 -0500

    Proyecto PetShop

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css';
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre"
+
+import { BrowserRouter as  Router} from 'react-router-dom';
+
+function App() {
+   /*FUNCIONES */
+
+
+
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
